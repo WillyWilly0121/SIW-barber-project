cd627be2fb0a57d55c52ca824462f55edb3165d8
buonhobo <naturytropik@gmail.com>
2023-06-01 16:22:39 +0200

    Added ottieniNumeroPrenotazioniPerData in service

--- src/main/java/it/uniroma3/siw/repository/PrenotazioneRepository.java
+++ src/main/java/it/uniroma3/siw/repository/PrenotazioneRepository.java
@@ -1,9 +1,13 @@
 package it.uniroma3.siw.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import it.uniroma3.siw.model.Prenotazione;
+import org.springframework.data.repository.query.Param;
 
 public interface PrenotazioneRepository extends CrudRepository<Prenotazione, Long>{
 
+    @Query(nativeQuery = true, value = "select data_prenotazione, count(*) from prenotazione where barbiere= :barberId group by data_prenotazione")
+    Iterable<Object[]> ottieniNumeroPrenotazioniPerData(@Param("barberId") Long barberId);
 }
--- src/main/java/it/uniroma3/siw/service/PrenotazioneService.java
+++ src/main/java/it/uniroma3/siw/service/PrenotazioneService.java
@@ -1,8 +1,28 @@
 package it.uniroma3.siw.service;
 
+import it.uniroma3.siw.repository.PrenotazioneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PrenotazioneService {
+    @Autowired
+    PrenotazioneRepository prenotazioneRepository;
 
+    Map<Date,Integer> ottieniNumeroPrenotazioniPerData(Long barberId) {
+        HashMap<Date,Integer> result = new HashMap<>();
+
+        for (Object[] tuple: prenotazioneRepository.ottieniNumeroPrenotazioniPerData(barberId)){
+            Date data = (Date) tuple[0];
+            Integer count= (Integer) tuple[1];
+
+            result.put(data,count);
+        }
+
+        return result;
+    }
 }

90a26251d109558f95bdf3d4ab71a78c978d0c82
WillyWilly0121 <franciosiwilliam01@gmail.com>
2023-06-01 14:48:00 +0200

    auth

---
+++ src/main/java/it/uniroma3/siw/model/Credentials.java
@@ -0,0 +1,77 @@
+package it.uniroma3.siw.model;
+
+import jakarta.persistence.OneToOne;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Credentials {
+	public static final String DEFAULT_ROLE = "DEFAULT";
+	public static final String ADMIN_ROLE = "ADMIN";
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+	private String username;
+	private String password;
+	private String role;
+	@OneToOne
+	private Utente utente;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public Utente getUser() {
+		return utente;
+	}
+	public void setUser(Utente user) {
+		this.utente = user;
+	}
+	public boolean isAdmin() {
+		if (this.role.equals(Credentials.ADMIN_ROLE)) {
+			return true;
+		}
+		return false;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(password, username);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Credentials other = (Credentials) obj;
+		return Objects.equals(password, other.password) && Objects.equals(username, other.username);
+	}
+
+}
--- src/main/java/it/uniroma3/siw/model/Utente.java
+++ src/main/java/it/uniroma3/siw/model/Utente.java
@@ -8,9 +8,12 @@ import jakarta.persistence.Id;
 @Entity
 public class Utente {
 
-    @Id
+	@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+	private String name;
+	private String surname;
+	private String email;
 
     public Long getId() {
         return id;
@@ -19,4 +22,28 @@ public class Utente {
     public void setId(Long id) {
         this.id = id;
     }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }

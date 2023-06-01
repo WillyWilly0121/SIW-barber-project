9671376c4b3e5ca844b9e7f9f4a253963f4cf147
WillyWilly0121 <franciosiwilliam01@gmail.com>
2023-06-01 15:16:28 +0200

    service and repository

---
+++ src/main/java/it/uniroma3/siw/repository/PrestazioneEffettuataRepository.java
@@ -0,0 +1,9 @@
+package it.uniroma3.siw.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.siw.model.PrestazioneEffettuata;
+
+public interface PrestazioneEffettuataRepository extends CrudRepository<PrestazioneEffettuata, Long>{
+
+}
